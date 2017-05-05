eb765149d158e07489e909491ec96508bfa8390d
LEAFERx <jerryleafer@outlook.com>
2017-05-05 20:28:21 +0800

    Create Ex1-16

---
+++ codes/Ex1-16.scm
@@ -0,0 +1,11 @@
+(define (odd? n)
+    (= (remainder n 2) 1))
+(define (sqaure x) (* x x))
+
+(define (fast-expt-iter base index result)
+    (cond ((= index 0) result)
+          ((odd? index) (fast-expt-iter base (- index 1) (* result base)))
+          (else (fast-expt-iter (sqaure base) (/ index 2) result))))
+
+(define (fast-expt base index)
+    (fast-expt-iter base index 1))
